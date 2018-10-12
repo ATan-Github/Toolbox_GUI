72ca5b48cc426d4a628c20b3078daf665d5ce090
Amanda Tan <tan.amanda.j@gmail.com>
2018-10-11 21:22:28 -0700

    practice so far

--- checkbutton_practice.py
+++ checkbutton_practice.py
@@ -1,5 +1,48 @@
-# """ Learning how to use checkbuttons """
+# """ Learning how to do things """
 
+# try:
+    # # python 3.x
+    # import Tkinter as tk
+
+# except ImportError:mand to continue
+    # #python 2.x
+    # import Tkinter as tk
+################################################
+from Tkinter import *
+import tkMessageBox
+import Tkinter
+
+top = Tkinter.Tk()
+CheckVar1 = IntVar()
+CheckVar2 = IntVar()
+C1 = Checkbutton(top, text = "Music", variable = CheckVar1, \
+                 onvalue = 1, offvalue = 0, height=3, \
+                 width = 20, selectcolor="red", activeforeground="blue", \
+                 image = '/Untitled/Users/Amanda1/gitRepos/Toolbox_GUI/flathead.jpg')
+C2 = Checkbutton(top, text = "Video", variable = CheckVar2, \
+                 onvalue = 1, offvalue = 0, height=3, \
+                 width = 20) 
+grid_rowconfigure(0, weight=1)
+grid_columnconfigure(0, weight=1)
+minsize(500, 500)
+
+C1.pack()
+C2.pack()
+top.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+##################################################
 # from Tkinter import *
 # master = Tk()
 # mb = Menubutton(master, text="CheckBox", relief=RAISED )
@@ -24,8 +67,9 @@
     # presence.append(1)
 # print(presence)
 ###################################################
-apple = ["mom", "dad", "poo"]
-f = open('test.txt', 'a')
-f.write('hello hello\n')
-f.writelines( "%s " % item for item in apple)
-f.close()
+# apple = ["mom", "dad", "poo"]
+# f = open('test.txt', 'a')
+# f.write('hello hello\n')
+# f.writelines( "%s " % item for item in apple)
+# f.close()
+#####################################################
